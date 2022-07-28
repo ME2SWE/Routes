486d1b6cfb4dcc6eb912f98221f832db53f2dd82
bonnie <arlolocos@gmail.com>
2022-07-28 15:29:30 -0400

    db added

---
+++ server/db.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const mongoURI = 'mongodb://localhost/watchList';
+
+mongoose.connect(mongoURI);
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'mongo connection error'));
+
+const baseSchema = {
+  name: {
+    type: String,
+    required: true
+  },
+  year: Number,
+  desc: String
+};
+
+const ShowSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  year: Number
+});
+const MovieSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  year: Number
+});
+
+let Show = mongoose.model('Show', ShowSchema);
+let Movie = mongoose.model('Movie', MovieSchema);
+
+
+const addShow = async(data) => {
+  try{
+    const {name, year} = data;
+    const show = await new Show({name, year}).save();
+  } catch (err) {
+    console.log('Error saving movie to db', err);
+  }
+}
+
+const addMovie = async(data) => {
+  try{
+    const {name, year} = data;
+    const movie = await new Movie({name, year}).save();
+  } catch (err) {
+    console.log('Error saving movie to db', err);
+  }
+}
+
+module.exports = {Show, Movie, addShow, addMovie};
