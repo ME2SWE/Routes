8a12be42ffc0cf04ad761d371de85570130196f4
bonnie <arlolocos@gmail.com>
2022-07-28 16:21:37 -0400

    controllers skeleton

--- server/controller.js
+++ server/controller.js
@@ -1,1 +1,29 @@
+require("dotenv").config();
+const key = process.env.API_KEY;
 const axios = require('axios');
+const fetch = require('node-fetch');
+const {Show, Movie, addShow, addMovie} = require('./db')
+
+// @desc    Get shows
+//@route    GET /shows/:name
+exports.getShows = (req, res, next) => {
+
+}
+
+// @desc    Get movies
+//@route    GET /movies
+exports.getMovies = (req, res, next) => {
+
+}
+
+// @desc    Save shows
+//@route    POST /shows
+exports.saveShow = async(req, res, next) => {
+
+}
+
+// @desc    Save movies
+//@route    POST /movies
+exports.saveMovie = async(req, res, next) => {
+
+}
