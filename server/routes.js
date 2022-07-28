163387d7ad73e561a1b7997fc1015a432e9028a7
bonnie <arlolocos@gmail.com>
2022-07-28 15:31:46 -0400

    router skeleton

---
+++ server/routes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+// const {
+
+// } = require('./controller');
+
+// router.route('/shows/:name')
+//   .get(getShows)
+
+// router.route('/shows')
+//   .post(saveShow)
+
+// router.route('/movies')
+//   .get(getMovies)
+//   .post(saveMovie)
+
+
+
+module.exports = router;
