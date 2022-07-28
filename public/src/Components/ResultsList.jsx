93011081b7c2373a3ccdf1bf0f7e0c85c3b97285
bonnie <arlolocos@gmail.com>
2022-07-28 15:11:32 -0400

    resultsList refactored

--- public/src/Components/ResultsList.jsx
+++ public/src/Components/ResultsList.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ListItem from './ListItem.jsx';
 
-const ResultsList = (props) => (
+const ResultsList = (props) => {
+return (
   <div id="results-container">
-    <h2 id="results-title" className="heading">Results</h2>
-    <ul id="list"></ul>
-</div>
-)
+      <h2 id="results-title" className="heading">Results</h2>
+      <ul id="list">
+        {props.shows.map((show, i) => (
+          <ListItem key={i} show={show} type={props.type}></ListItem>
+        ))}
+      </ul>
+  </div>
+)}
 
 
 export default ResultsList;
